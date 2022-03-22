5cca68a94a18daafa22fb7f6b641440909a18237
Sergei Boreiko <hsvpass@gmail.com>
2022-03-22 16:24:42 +0400

    feat: add pet projects

--- main.js
+++ main.js
@@ -1,1 +1,57 @@
-console.log( "score 100/100  Можно номинировать в лучшие работы, я старался")
+console.log('running')
+
+const modal = document.querySelector('#modalWindow')
+const iframe = document.querySelector('#iframe-project')
+const closeEl = document.querySelector('.modal-window-close')
+const modalTitle = document.querySelector('.modal-window-title');
+const wrapper = document.querySelector('.wrapper')
+
+const closeModal = (event) =>{
+
+ 
+  if( event.target === closeEl){
+    document.body.style.overflow = 'scroll'
+    wrapper.style.filter = 'none';
+    modal.style.display = 'none'
+    iframe.src = ""
+  }else {
+    event.sto
+    console.log(event.path)
+  }
+
+  
+}
+
+
+const showModal = (card) => {
+
+
+  const projectSrc = {
+    gitproject: 'https://gitproject.vercel.app/',
+    momentum : 'https://momentum-jsbrownn.netlify.app/',
+    museum : 'https://rolling-scopes-school.github.io/jsbrownn-JSFE2021Q3/museum/',
+    tictactoe: 'https://jsbrownn.github.io/tic-tac-toe/'
+  }
+
+ 
+
+  
+  modalTitle.textContent = card.id;
+  modal.style.display = 'block';
+  document.body.style.overflow = 'hidden';
+  wrapper.style.filter = 'brightness(0.2)';
+  iframe.src = projectSrc[card.id];
+  
+  
+  document.addEventListener('click', (event) =>{
+    closeModal(event)
+  })
+
+}
+
+const cards = document.querySelectorAll('.projects_card')
+cards.forEach( card => {
+  card.addEventListener('click', () =>{
+    showModal(card)
+  })
+})
